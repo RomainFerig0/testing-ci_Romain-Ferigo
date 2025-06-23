a0498ae65f6cad478c5c3d10ca132b9fa8ae6828
RomainFerig0 <104143932+RomainFerig0@users.noreply.github.com>
2025-06-23 15:59:00 +0200

    TP 4 begin

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+import './index.css';
+
+const express = require('express');
+const app = express();
+const PORT = 5000;
+
+app.get('/api', (req, res) => {
+  res.json({ message: "Hello from backend!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
